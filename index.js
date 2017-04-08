d22cb9232c1285fdcf00d32a5336aca8715e1719
Mike Bartner <m.bartner16@hotmail.com>
2017-04-08 19:16:02 -0400

    stuff

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var T = new twit(config);
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log(window.location.hostname);
 });
-
 
 /*var tweetObj = [];
 
