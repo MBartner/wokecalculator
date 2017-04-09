a0344f9fa0d1a76f3811a84497a926ed2c1eba5a
Mike Bartner <m.bartner16@hotmail.com>
2017-04-09 01:33:30 -0400

    Stuff

--- Hide/README.md
+++
@@ -1,2 +0,0 @@
-# wokecalculator
-Determine user's social awareness based on their social media data.
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
