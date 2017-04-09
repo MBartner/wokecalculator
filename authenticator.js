9f50f400522e845570eab9b4c2ae3aaff7cfff8e
Mike Bartner <m.bartner16@hotmail.com>
2017-04-08 21:29:14 -0400

    Stuff

--- authenticator.js
+++ authenticator.js
@@ -10,3 +10,28 @@ var oauth = new OAuth(
 	config.oauth_callback,
 	config.oauth_signature
 );
+
+module.exports = {
+	redirectToTwitterLoginPage: function(req, res){
+		oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+			if(error){
+				console.log(error);
+				res.send("Authentication failed!");
+			} else {
+				res.cookie('oauth_token', oauth_token, {httpOnly: true});
+				res.cookie('oauth_token_secret', oauth_token_secret, {httpOnly: true});
+				res.redirect(config.authorize_url + "?oauth_token=" + oauth_token);
+			}
+		});
+	},
+	authenticate: function(req, res, cb) {
+		if(!(req.cookies.oauth_token && req.cookies.oauth_token_secret && req.query.oauth_verifier)) {
+			return cb("Request does not have all required keys");
+		}
+
+		res.clearCookie('oauth_token');
+		res.clearCookie('oauth_token_secret');
+
+		cb();
+	}
+}
