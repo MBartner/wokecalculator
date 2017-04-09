a49d05ae12598ac6bc3d48cbd75a05720877868f
Mike Bartner <m.bartner16@hotmail.com>
2017-04-08 20:01:40 -0400

    Stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var twit = require('twit');
 var config = require('./config');
+var OAuth = require('OAuth');
 
 var app = express();
 
@@ -39,3 +40,52 @@ function organizeData(err, tweets, printer){
 		console.log(tweetObj[i].date + ":" + tweetObj[i].text + "\n");
 	}
 }
+
+var oa = new OAuth(
+	"https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"5nCxdlyOaKPDO872Ip3JY1N46",
+	"WyEF92yfazPVgQ7W6Z8F3FRVvlWuMGXPVxBggTwYqeImFDYN73",
+	"1.0",
+	"/auth/twitter/callback",
+	"HMAC-SHA1"
+);
+
+app.get('/auth/twitter', function(req, res){
+	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+		if (error) {
+			console.log(error);
+			res.send("yeah no. didn't work.")
+		}
+		else {
+			req.session.oauth = {};
+			req.session.oauth.token = oauth_token;
+			console.log('oauth.token: ' + req.session.oauth.token);
+			req.session.oauth.token_secret = oauth_token_secret;
+			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+			res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+	}
+	});
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+	if (req.session.oauth) {
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth = req.session.oauth;
+
+		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+		function(error, oauth_access_token, oauth_access_token_secret, results){
+			if (error){
+				console.log(error);
+				res.send("yeah something broke.");
+			} else {
+				req.session.oauth.access_token = oauth_access_token;
+				req.session.oauth,access_token_secret = oauth_access_token_secret;
+				console.log(results);
+				res.send("worked. nice one.");
+			}
+		}
+		);
+	} else
+		next(new Error("you're not supposed to be here."))
+});
